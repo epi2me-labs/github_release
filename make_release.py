d54479ab5ac0b3a6d524845c58dd754bf502aa68
Chris Wright <chris.wright@nanoporetech.com>
2020-12-10 16:16:09 +0000

    Add changelog parsing

--- make_release.py
+++ make_release.py
@@ -1,8 +1,28 @@
 import argparse
+import re
 import sys
 import textwrap
 
 from github import Github
+
+def changelog_parser(changelog):
+    releases = dict()
+    version = None
+    buf = list()
+    with open(changelog, 'r') as log:
+        for line in log.readlines():
+            m = re.match('## \[(.*)\]', line)
+            if m:
+                if version is not None:
+                    releases[version] = ''.join(buf)
+                    buf = list()
+                version = m.group(1)
+            else:
+                buf.append(line)
+        if version is not None:
+            releases[version] = ''.join(buf)
+    return releases
+
 
 def main():
     parser = argparse.ArgumentParser('Make a github release')
@@ -18,11 +38,21 @@ def main():
         'token', help='Github OAuth token')
     parser.add_argument(
         '--artifacts', nargs='+', help='Filepaths or artifacts to upload.')
+    parser.add_argument(
+        '--update', action='store_true',
+        help='Update (delete and recreate) an existing release.')
     args = parser.parse_args()
     
     g = Github(args.token)
     user = g.get_user()
- 
+
+    changelogs = changelog_parser(args.changelog)
+    try:
+        message = changelogs[args.tag]
+    except KeyError:
+        print("No entry for '{}' in changelog file.".format(args.tag))
+        sys.exit(1)
+
     found = False
     for repo in user.get_repos():
         if repo.name == args.repository:
@@ -32,23 +62,33 @@ def main():
         print("Not found repository '{}'.".format(args.repository))
         sys.exit(1)
 
-    print(repo)
-    # TODO: make message
-    message = args.changelog
     print("Creating release: {}.".format(args.tag))
     print(textwrap.indent(message, "    "))
     print()
     try:
-        release = repo.create_git_release(args.tag, args.tag, message)
+        if not args.update:
+            release = repo.create_git_release(args.tag, args.tag, message)
+            print("Created release.")
+        else:
+            release = repo.get_release(args.tag)
+            release.delete_release()
+            release = repo.create_git_release(args.tag, args.tag, message)
+            print("Deleted and recreated release.")
     except Exception as e:
         print(e)
-        print("Failed to create release, does the tag exist?")
+        print("Failed to create release:")
+        print(" * does the tag exist?\n * does the user have access?")
         sys.exit(1)
+    print()
 
-    if artifacts is not None:
+    if args.artifacts is not None:
+        print("Uploading artifacts")
         for artifact in args.artifacts:
-            print("Adding {}.".format(artifact))
-            release.upload_asset(asset)
+            print(" Adding {}.".format(artifact))
+            release.upload_asset(artifact)
+    else:
+        print("No artifacts to upload.")
+
 
 if __name__ == "__main__":
     main()
